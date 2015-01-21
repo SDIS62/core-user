e04d9a4d162e062266277dbafc874fa9f43adb74
Kévin DUBUC <kevindubuc62@gmail.com>
2015-01-21 17:49:35 +0100

    Ajout d'un profil manquant

---
+++ src/Entity/Profile/Sdis/PersonnelTechniqueSdisProfile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SDIS62\Core\User\Entity\Profile\Sdis;
+
+use SDIS62\Core\User\Entity\Profile\SdisProfile;
+
+class PersonnelTechniqueSdisProfile extends SdisProfile
+{
+    /**
+     * Type du profil
+     *
+     * @var string
+     */
+    protected $type = 'personnel_technique';
+}
